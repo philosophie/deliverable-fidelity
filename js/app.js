f0ca8dd7e1145d2c632c45fb6beb8d294b136d34
Jamie <jamie@gophilosophie.com>
2014-09-05 14:08:09 -0400

    included custom Magellan parameters in app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
+
+$(document).foundation({
+"magellan-expedition": {
+  active_class: 'active', // specify the class used for active sections
+  threshold: 0, // how many pixels until the magellan bar sticks, 0 = auto
+  destination_threshold: -400, // pixels from the top of destination for it to be considered active
+  throttle_delay: 50, // calculation throttling to increase framerate
+  fixed_top: 0, // top distance in pixels assigned to the fixed element on scroll
+}
+});
